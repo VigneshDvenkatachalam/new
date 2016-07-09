cbee7cb87523f158f816c13c71ef3171c047e23d
VigneshDvenkatachalam <vigneshvenkatachalam95@gmail.com>
2016-07-08 21:30:29 -0700

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,12 @@
+
+public class Reverse {
+
+	public static void main(String[] args) {
+		String s1="HELLOW";
+		String s2=new StringBuffer(s1).reverse().toString();
+		System.out.println(s2);
+		
+
+	}
+
+}
