53dcb68ac99e7f9ea1daf2dd5c8d88c545a7f871
VigneshDvenkatachalam <vigneshvenkatachalam95@gmail.com>
2016-07-09 10:48:58 +0530

    Add files via upload

---
+++ Reversedigits.java
@@ -0,0 +1,12 @@
+
+public class Reversedigits {
+
+	public static void main(String[] args) {
+		String s1="123";
+		String s2=new StringBuffer(s1).reverse().toString();
+		System.out.println(s2);
+		
+
+	}
+
+}
